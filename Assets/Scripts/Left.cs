9829cf85f7547ec3e0a07b88f6aee760576208cb
Javier <mrvizious@gmail.com>
2021-01-29 22:43:56 +0100

    Left class created

---
+++ Assets/Scripts/Left.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Left : MonoBehaviour
+{
+    public Animation animation;
+    public ItemSO item;
+    public TraitSO trait;
+}
---
+++ Assets/Scripts/LevelController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour
+{
+    private List<Combination> combinations;
+
+    private int leftIndex, rightIndex;
+
+    public void ChangeGiverRight()
+    {
+
+    }
+}
