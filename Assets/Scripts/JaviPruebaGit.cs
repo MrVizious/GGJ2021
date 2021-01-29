b061cbd716b61639edbf393b58bff9429f3996bf
Javier <mrvizious@gmail.com>
2021-01-29 15:29:19 +0100

    Javi prueba git

--- Assets/Scripts/JaviPruebaGit.cs
+++ Assets/Scripts/JaviPruebaGit.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PruebaGit : MonoBehaviour
+public class JaviPruebaGit : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
