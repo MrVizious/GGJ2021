30579b2833e90a966862e28f3d1256ddd14b4f40
Álvaro Grafiá <avox97@gmail.com>
2021-01-29 15:31:09 +0100

    Prueba resolución conflicto

--- Assets/Scripts/JaviPruebaGit.cs
+++ Assets/Scripts/JaviPruebaGit.cs
@@ -7,12 +7,8 @@ public class JaviPruebaGit : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Riera comeme la polla");
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
