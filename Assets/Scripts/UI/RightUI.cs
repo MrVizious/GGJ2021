81727ada392905c1aababca1d6d026c5990d866b
Javier <mrvizious@gmail.com>
2021-01-30 14:24:05 +0100

    UI skeleton started

---
+++ Assets/Scripts/UI/RightUI.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RightUI : MonoBehaviour
+{
+    // public LevelDataSO data;
+    public Image itemImage;
+    public Animation animalAnimation;
+    public void UpdateGraphics()
+    {
+        // TODO: Update images according to data
+    }
+}
---
+++ Assets/Scripts/UI/StorageUI.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StorageUI : MonoBehaviour
+{
+    // public LevelDataSO data;
+    public Image[] storageImages;
+
+    public void UpdateGraphics()
+    {
+        // TODO: Update images according to data
+    }
+}
