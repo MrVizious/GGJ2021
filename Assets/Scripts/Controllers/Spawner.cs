399bff057e77e096ed7d17d4cfa023cb72c56e15
Javier <mrvizious@gmail.com>
2021-01-30 16:11:30 +0100

    Spawner and difficulty levels

--- Assets/Scripts/Controllers/Spawner.cs
+++ Assets/Scripts/Controllers/Spawner.cs
@@ -4,8 +4,51 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    private float time = 0f;
+    public enum LevelUpMethod
+    {
+        Time,
+        Score
+    }
+    public LevelController levelController;
+    public SpawnerLevels levels;
+    public LevelUpMethod levelUpMethod;
+    public Timer timer;
+    // public Score score;
+
+    private int currentLevel;
+    private float lastSpawnTimeStamp = 0f;
+
     private void Start()
     {
+        lastSpawnTimeStamp = 0f;
+        currentLevel = 0;
     }
+
+    private void Update()
+    {
+        switch (levelUpMethod)
+        {
+            case LevelUpMethod.Time:
+                if (timer.secondsSinceStart
+                    > levels.levels[currentLevel].maxPoint)
+                {
+                    currentLevel++;
+                }
+                if (timer.secondsSinceStart
+                    >= lastSpawnTimeStamp + levels.levels[currentLevel].secondsBetweenSpawn)
+                {
+                    for (int i = 0; i < levels.levels[currentLevel].numberOfCombinationsToSpawn; i++)
+                    {
+                        levelController.AddCombination();
+                    }
+                }
+                break;
+
+                // TODO: Level up using score
+                //case LevelUpMethod.Score:
+                //    if
+        }
+    }
+
+
 }
