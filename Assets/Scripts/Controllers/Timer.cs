1767bedbeeeaea4af48c944c86bee50a06d6f019
Javier <mrvizious@gmail.com>
2021-01-30 14:50:19 +0100

    Timer implemented

---
+++ Assets/Scripts/Controllers/Timer.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Timer : MonoBehaviour
+{
+    [Min(0.001f)]
+    public float maxSeconds = 5f;
+    public float remainingSecondsPercentage = 1f;
+    public bool running = true;
+    public UnityEvent onTimeUp;
+
+
+    private float remainingSeconds;
+
+
+    private void Start()
+    {
+        ResetTimer();
+    }
+
+    private void Update()
+    {
+        if (running)
+        {
+            remainingSeconds = Mathf.Clamp(
+                remainingSeconds - Time.deltaTime,
+                0f,
+                maxSeconds
+            );
+            UpdatePercentage();
+            if (remainingSeconds <= Mathf.Epsilon)
+            {
+                onTimeUp.Invoke();
+                StopTimer();
+            }
+        }
+    }
+
+    public void StartTimer()
+    {
+        setRunning(true);
+    }
+    public void StopTimer()
+    {
+        setRunning(false);
+    }
+    public void ToggleTimerRunning()
+    {
+        setRunning(!running);
+    }
+
+    public void setRunning(bool newValue)
+    {
+        running = newValue;
+    }
+
+    public void ResetTimer()
+    {
+        remainingSeconds = maxSeconds;
+        UpdatePercentage();
+    }
+
+    private void UpdatePercentage()
+    {
+        remainingSecondsPercentage = Mathf.Clamp(
+            remainingSeconds / maxSeconds,
+            0f,
+            maxSeconds
+        );
+    }
+}
---
+++ Assets/Scripts/UI/TimerUI.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerUI : MonoBehaviour
+{
+    public Image image;
+    public Timer timer;
+
+    private void Start()
+    {
+        image.type = Image.Type.Filled;
+        image.fillMethod = Image.FillMethod.Radial360;
+        image.fillOrigin = 2; //This equals to Top
+        image.fillClockwise = false;
+    }
+
+    private void Update()
+    {
+        image.fillAmount = timer.remainingSecondsPercentage;
+    }
+}
